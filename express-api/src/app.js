f12e361ee7df318a9d7019a5fa11f2b2fc086979
ENG19CS0006-Abhay <eng19cs0006.abhay@gmail.com>
2026-05-15 23:12:29 +0530

    CORS added

--- express-api/src/app.js
+++ express-api/src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const cors = require("cors");
+
 const healthRoutes = require("./routes/health.routes");
 const userRoutes = require("./routes/user.routes");
 const financeRoutes = require("./routes/finance.routes");
@@ -7,7 +9,19 @@ const assetRoutes = require("./routes/asset.routes");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://findash-jency-abhay.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+
 app.use("/api", healthRoutes);
 app.use("/api", userRoutes);
 app.use("/api", financeRoutes);
